558c32c7832c03ba725acc2d0ac92c5b18fc80cd
codewithdevu <divyanshyadav8302@gmail.com>
2026-07-09 14:09:32 +0530

    feat:  make components and changes in json and page.tsx

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,24 @@
 "use client"
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useState } from "react"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    )
-  }
+
+export default function page() {
+
+  const [username, setUsername] = useState("")
+  const [usernameMessage, setUsernameMessage] = useState("")
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  
+
+
+  
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-orange-500 hover:bg-orange-600 text-white font-medium px-6 py-2.5 m-4 rounded-md shadow-md transition-all duration-200 block mx-auto w-fit" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div>
+      page
+    </div>
   )
 }
