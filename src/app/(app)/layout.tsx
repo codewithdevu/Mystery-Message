80c3ae2484307dfa91e36ddcdbcba348feea7f12
codewithdevu <divyanshyadav8302@gmail.com>
2026-07-20 13:32:20 +0530

    feat: fix message acceptance logic, add delete message routing, and update navbar layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,17 +1,7 @@
-import AuthProvider from "@/context/AuthProvider"; // Adjust this import path to match your project
-
 export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <AuthProvider>
-      {/* 
-        NO <html> OR <body> TAGS HERE! 
-        They are already provided by your main root layout.
-      */}
-      {children}
-    </AuthProvider>
-  );
+  return <>{children}</>;
 }
